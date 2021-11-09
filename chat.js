3a9c5c56e3e2327a76992e3fb3745782482cb9f2
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-11-09 11:13:54 +0100

    sfhrhgrj

--- chat.js
+++ chat.js
@@ -1,12 +1,12 @@
-let query1 = document.getElementsByTagName('form');
-// console.log(query1);
-let query2 = document.getElementsByTagName('select');
-console.log(query2);
+// let query1 = document.getElementsByTagName('form');
+// // console.log(query1);
+// let query2 = document.getElementsByTagName('select');
+// console.log(query2);
 
 
 
 
-  query2.addEventListener('click', function () {
-    // document.getElementsByName("form").innerHTML = 'Your order has been submited';
-    alert('Your order has been submited');
-});
+//   query2.addEventListener('click', function () {
+//     // document.getElementsByName("form").innerHTML = 'Your order has been submited';
+//     alert('Your order has been submited');
+// });
